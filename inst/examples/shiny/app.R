2445ca6307396eff5fc9d3b6b289d4fb5fe3f259
timelyportfolio <kent.russell@timelyportfolio.com>
2016-06-23 16:48:32 -0500

    convert old shiny example to crosstalk

--- inst/examples/shiny/app.R
+++ inst/examples/shiny/app.R
@@ -26,10 +26,11 @@ ui <- dashboardPage(
 server = function(input, output, session) {
   ###generate a data set to use
   data( diamonds, package = "ggplot2" )
-  DiamondData <-   diamonds %>%
-    mutate( carat = cut(carat,breaks = c(0,1,2,3,4,5), right =F) ) %>%
+  dsd <-  diamonds %>%
+    mutate( carat = cut(carat,breaks = c(0,1,2,3,4,5,6), right =T) ) %>%
     group_by( carat ) %>%
-    summarise_each(funs(mean),-carat)
+    summarise_each(funs(mean),-c(carat,cut,color,clarity)) %>%
+    SharedData$new()
 
   ###Add an ID field called "DataId" so the js can identify which column is your
   ###unique identifier when brushed and exported back to shiny through input$id_brushed_row_names
@@ -38,7 +39,7 @@ server = function(input, output, session) {
   ###standard parcoords plot in shiny
 
   output$DiamondPlot <- renderParcoords({
-    parcoords(DiamondData,rownames= T,
+    parcoords(dsd,rownames= T,
               color = list(colorScale = htmlwidgets::JS('d3.scale.category10()'),
                             colorBy = "carat"),
               brushMode = "2D-strums")
@@ -47,8 +48,13 @@ server = function(input, output, session) {
   ###Here we can access the variable input$id_rows to determine which are selected
   ###we display these results in a table
   output$SelectedData <- renderDataTable({
-    ids <- rownames(DiamondData) %in% input$DiamondPlot_brushed_row_names
-    DiamondData[ids,]
+    df <- dsd$data(TRUE)
+
+    if(any(is.na(df$selected_))) {
+      df
+    } else {
+      df %>% filter(selected_) %>% select(-selected_)
+    }
   })
 
 }
